246c8a6c4ffd33342160f7852c37f219514d1c95
Noah Lundquist <nalundquist@gmail.com>
2022-10-21 13:57:14 -0700

    bugfix: somehow only added half of startup, now fully present

--- PierreTreats/Startup.cs
+++ PierreTreats/Startup.cs
@@ -24,6 +24,37 @@ namespace PierreTreats
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
-		}
-	}
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<PierreTreatsContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+              .AddEntityFrameworkStores<PierreTreatsContext>()
+              .AddDefaultTokenProviders();  
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+
+      app.UseAuthentication();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(routes =>
+      {
+        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      app.UseStaticFiles();
+      
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello World!");
+      });
+    }
+  }
 }
